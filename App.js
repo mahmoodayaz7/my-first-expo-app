687fcfe345013c17c01fc81579529a86cf0558cb
Mehmood Ayaz <mahmoodayaz7@gmail.com>
2022-01-22 16:55:18 +0500

    Delete Item and Add Item Components Added

--- App.js
+++ App.js
@@ -1,24 +1,58 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, FlatList} from 'react-native';
+import Header from './components/Header';
+import ListItem from './components/ListItem';
+import AddItem from './components/AddItem';
 
-export default function App() {
+
+const App = () => {
+  
+  const [items, setItems] = useState([
+    {id: 1, text : 'Milk'},
+    {id: 2, text : 'Eggs'},
+    {id: 3, text : 'Bread'},
+    {id: 4, text : 'Juice'},
+  ]);
+
+  const deleteItem = (id) => {
+   return setItems(prevItems => {
+     return prevItems.filter(item => item.id != id);
+   }); 
+  }
+
+  const addItem = (text) => {
+    if(text != ''){
+      setItems(prevItems => {
+        return [{id: Math.random().toString(), text}, ...prevItems];
+      });
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello World</Text>
-      <StatusBar style="auto" />
+      
+      <Header title="Shopping List" />
+
+      <AddItem addItem={addItem} />
+      
+      <FlatList 
+        data={items} 
+        renderItem={({item}) => 
+        //<Text style={styles.item}>{item.text}</Text>
+        <ListItem item={item} deleteItem={deleteItem}/>
+      }/>      
+    
     </View>
+    
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 1,    
+    height:80,
   },
-  text: {
-    color: 'darkslateblue',
-    fontSize: 30,
-  }
 });
+
+
+export default App;
---
+++ components/Header.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const Header = ({title}) => {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.text}>{title}</Text>
+    </View>
+  );
+};
+
+Header.defaultProps = {
+    title: 'Shopping List',
+}
+
+const styles = StyleSheet.create({
+  header: {
+    height: 80,
+    padding: 15,
+    backgroundColor: 'darkslateblue',        
+  },  
+  text: {
+    color: 'white',
+    fontSize:23,
+    textAlign: 'center',
+    marginTop:15,
+    }
+});
+
+
+export default Header;
