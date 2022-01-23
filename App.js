6cc801fbbdeec1778db5ce0278bbab09fd9f87b9
Mehmood Ayaz <mahmoodayaz7@gmail.com>
2022-01-23 16:00:18 +0500

    Add Alert Message

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {View, Text, StyleSheet, FlatList} from 'react-native';
+import {View, Text, StyleSheet, FlatList, Alert} from 'react-native';
 import Header from './components/Header';
 import ListItem from './components/ListItem';
 import AddItem from './components/AddItem';
@@ -21,10 +21,14 @@ const App = () => {
   }
 
   const addItem = (text) => {
-    if(text != ''){
+    if(text != '')
+    {
       setItems(prevItems => {
         return [{id: Math.random().toString(), text}, ...prevItems];
       });
+    }
+    else {
+      Alert.alert('Error', 'Please enter an item', [{text: 'Ok'}]);
     }
   }
 
